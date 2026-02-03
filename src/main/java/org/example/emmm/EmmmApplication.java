bbaf3d9a9824b5485c8cf30f1d46ade13dda8b1f
김민제 <kimminje@gimminje-ui-MacBookPro.local>
2026-02-03 15:46:55 +0900

    init Emmm

---
+++ src/main/java/org/example/emmm/EmmmApplication.java
@@ -0,0 +1,13 @@
+package org.example.emmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emmm/EmmmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
