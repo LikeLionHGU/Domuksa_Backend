84faa99bb9e226c2ed949b55fb898c849e8577ce
김민제 <kimminje@gimminje-ui-MacBookPro.local>
2026-02-05 15:56:21 +0900

    [feat] make complete & running Rooms in User

--- src/main/java/org/example/emmm/domain/Room.java
+++ src/main/java/org/example/emmm/domain/Room.java
@@ -45,6 +45,4 @@ public class Room{
 
     private String state;
 
-
-
 }
--- src/main/java/org/example/emmm/repository/UserRoomRepository.java
+++ src/main/java/org/example/emmm/repository/UserRoomRepository.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 public interface UserRoomRepository extends JpaRepository<UserRoom,Long> {
     Optional<UserRoom> findByUserAndRoom(User user, Room room);
     List<UserRoom> findAllByRoom(Room room);
+
+    List<UserRoom> findAllByUserId(Long id);
 }
